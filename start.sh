4b099c8441be68895720cf28bcec4703f622f2c8
enisberk <EnisBerk@users.noreply.github.com>
2019-07-23 14:25:49 -0400

    print python -v

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 virtualenv -p /usr/bin/python3 py3env
 source py3env/bin/activate
+python -V
 pip install scipy requests pillow lmdb opencv-python
 apt update && apt install -y libsm6 libxext6
 apt-get install -y libsm6 libxrender1 libfontconfig1
