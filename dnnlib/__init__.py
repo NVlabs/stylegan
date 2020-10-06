0c5a6a8cc78ca829bde657657ac65e7c6f788da4
jpgard <joshua.patrick.gardner@gmail.com>
2020-10-06 11:05:21 -0700

    change module structure to resolve errors with relative imports

--- dnnlib/__init__.py
+++ dnnlib/__init__.py
@@ -5,16 +5,16 @@
 # http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
 # Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
-from . import submission
+from dnnlib import submission
 
-from .submission.run_context import RunContext
+from dnnlib.submission.run_context import RunContext
 
-from .submission.submit import SubmitTarget
-from .submission.submit import PathType
-from .submission.submit import SubmitConfig
-from .submission.submit import get_path_from_template
-from .submission.submit import submit_run
+from dnnlib.submission.submit import SubmitTarget
+from dnnlib.submission.submit import PathType
+from dnnlib.submission.submit import SubmitConfig
+from dnnlib.submission.submit import get_path_from_template
+from dnnlib.submission.submit import submit_run
 
-from .util import EasyDict
+from dnnlib.util import EasyDict
 
 submit_config: SubmitConfig = None # Package level variable for SubmitConfig which is only valid when inside the run function.
