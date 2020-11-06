a16b7895e5d881f22227f0084e5c20d6b8187f73
jpgard <joshua.patrick.gardner@gmail.com>
2020-11-05 16:24:50 -0800

    add some print statements for debugging

--- dnnlib/tflib/network.py
+++ dnnlib/tflib/network.py
@@ -199,6 +199,9 @@ class Network:
 
     def get_output_for(self, *in_expr: TfExpression, return_as_list: bool = False, **dynamic_kwargs) -> Union[TfExpression, List[TfExpression]]:
         """Construct TensorFlow expression(s) for the output(s) of this network, given the input expression(s)."""
+        print("[DEBUG] in_expr: {}".format(str(in_expr)))
+        print("[DEBUG] len(in_expr): {}".format(len(in_expr)))
+        print("[DEBUG] self.num_inputs: {}".format(self.num_inputs))
         assert len(in_expr) == self.num_inputs
         assert not all(expr is None for expr in in_expr)
 
